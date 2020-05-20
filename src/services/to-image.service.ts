5a10b21dc81e9ec4b502de3c92de7dda94f68240
Roman <romanskycoding@gmail.com>
2020-05-20 19:10:50 +0300

    Update to-image.service.ts

--- src/services/to-image.service.ts
+++ src/services/to-image.service.ts
@@ -1,18 +1,29 @@
 import { saveAs } from 'file-saver';
 import html2canvas from 'html2canvas';
+import axios from 'axios';
+import { PictureAsPdf } from '@material-ui/icons';
 
+const googleApiImg = `https://maps.googleapis.com/maps/api/streetview?size=400x400&location=47.5763831,-122.4211769&fov=80&heading=70&pitch=0&key=AIzaSyDUIWsBLkvIUwzLHMHos9qFebyJ63hEG2M`;
+const imgGoogle = {};
+const fetchImgUrl = async () => {
+  let fetchedData = await axios.get(googleApiImg).then((data) => console.log(data.config.url));
+  return fetchedData;
+};
 
 const widgetToJpeg = (fileName: string, widgetElement: HTMLElement) => {
   html2canvas(widgetElement, {
-    useCORS: true,  // to allow loading maps
+    useCORS: true, // to allow loading maps
     imageTimeout: 3000,
     ignoreElements: (AnyWayButtton) => false,
   })
     .then(function (canvas) {
-      console.log(canvas);
+      const dataUrl = canvas.toDataURL();
+      const googleImg = fetchImgUrl();
+      // console.log(canvas);
       return canvas.toDataURL('image/png', 1.0);
     })
     .then(function (blob) {
+      // console.log(`This is a blob: ${blob}`);
       saveAs(blob, `${fileName}.png`);
     });
 };
